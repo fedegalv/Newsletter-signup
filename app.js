e653360c427a5ffbdb9bacd536dbf1a903270721
Federico G <fedegv.4991@gmail.com>
2019-11-04 15:50:57 -0300

    Finish web app

--- app.js
+++ app.js
@@ -4,11 +4,61 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.urlencoded({extenden: true}));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
 
 
+app.post("/", function(req, res){
+  var name = req.body.userName;
+  var lastname = req.body.userLastName;
+  var email = req.body.userEmail;
 
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name,
+          LNAME: lastname
+        }
+      }
+    ]
+  };
+  var jsonData= JSON.stringify(data);
+
+  var options = {
+     url: "https://us5.api.mailchimp.com/3.0/lists/1dc8f52110",
+     method: "POST",
+     headers: {
+       "Authorization": "fedegv 779f7a65bd8411a156a8ebb0732acf66-us5"
+     },
+     //body: jsonData
+  };
+  request(options, function(error, response, body){
+    if (error) {
+      console.log(error);
+      // res.send("An error has ocurred");
+      res.sendFile(__dirname +"/failure.html");
+    }
+    else {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      }
+      else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
 
 
 
@@ -19,3 +69,11 @@ app.use(bodyParser.urlencoded({extenden: true}));
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
+
+
+// API KEY
+// 779f7a65bd8411a156a8ebb0732acf66-us5
+
+// 1dc8f52110
+// https://us5.admin.mailchimp.com/lists/members?id=217013
